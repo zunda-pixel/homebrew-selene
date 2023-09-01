128add7afc49db26cfb0541a0962e96258b62b4f
zunda <47569369+zunda-pixel@users.noreply.github.com>
2023-09-01 18:52:57 +0900

    Update selene.rb

--- Formula/selene.rb
+++ Formula/selene.rb
@@ -1,5 +1,5 @@
 class Selene < Formula
-  desc "Formatting technology for Swift source code"
+  desc "Generation Swift source code from Env file"
   homepage "https://github.com/zunda-pixel/selene"
   url "https://github.com/zunda-pixel/selene.git",
       tag:      "1.1.1",
@@ -13,31 +13,20 @@ class Selene < Formula
     strategy :github_latest
   end
 
-  #bottle do
-  #  sha256 cellar: :any_skip_relocation, arm64_ventura:  "74c93fb3ea0ff22fcb2ccf43328047577baa4f37637a8649f98599796096bb38"
-  #  sha256 cellar: :any_skip_relocation, arm64_monterey: "80cc1fc3ba11cc6b4ddfc41d92e6ec5bbe74265bfd8bef28e2af1200c693fb61"
-  #  sha256 cellar: :any_skip_relocation, ventura:        "ad55a120d42363640a46cb75b1d18dbb3569dcfc9d3ac08f5a0a8372f696e193"
-  #  sha256 cellar: :any_skip_relocation, monterey:       "a88400e4fef158f06820e9375c8552e48d0b54997a100671e3db8458c66e1779"
-  #  sha256                               x86_64_linux:   "bd72a01ee943c12052f9dc34f2bd1952c4ca2d45e7fcc9fa42257a811b689c5f"
-  #end
-
-  # The bottles are built on systems with the CLT installed, and do not work
-  # out of the box on Xcode-only systems due to an incorrect sysroot.
-  pour_bottle? only_if: :clt_installed
-
-  depends_on xcode: ["14.0", :build]
-
   uses_from_macos "swift"
 
   def install
     system "swift", "build", "--disable-sandbox", "-c", "release"
     bin.install ".build/release/selene"
-    #doc.install "Documentation/Configuration.md"
   end
 
   test do
-    system "false"
-    #(testpath/"test.swift").write " print(  \"Hello, World\"  ) ;"
-    #assert_equal "print(\"Hello, World\")\n", shell_output("#{bin}/swift-format test.swift")
+    (testpath/".env").write <<~EOS
+      key1=value1
+      #key2=value2
+      key3=value3=value3
+    EOS
+    shell_output("#{bin}/selene SecureEnv .env SecureEnv.swift")
+    assert_predicate testpath/"SecureEnv.swift", :exist?
   end
 end
